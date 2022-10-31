4e2168de24259dbda65dc49a9a49f0f7335711e1
makskostyshen <maxxxymko2003@gmail.com>
2022-10-31 23:35:39 +0200

    :tada: Initial commit

---
+++ src/main/java/org/acme/demo/LeomatchDataExtractorApplication.java
@@ -0,0 +1,13 @@
+package org.acme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeomatchDataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeomatchDataExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/demo/LeomatchDataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeomatchDataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
